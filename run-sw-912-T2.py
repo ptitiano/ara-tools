a327e1b7055e9b6d9d311dcfae2b080f8019e0fd
Patrick Titiano <ptitiano@baylibre.com>
2015-08-27 20:25:52 +0200

    add T2-specific script
    Create 'run-sw-912-T2.py' to call 'sw-912-driver.py' with predefined T2 arguments.
    So that T2 scenario can be reproduce always with the same options.
    
    Signed-off-by: Patrick Titiano <ptitiano@baylibre.com>

---
+++ run-sw-912-T2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+
+from os import system
+import argparse
+
+def main():
+    # Parse arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--baudrate',
+                        help='baud rate of SVC/APB tty')
+    parser.add_argument('host', help='IP/hostname of target AP')
+    parser.add_argument('-s', '--size', default=500, help='Packet Size')
+    parser.add_argument('-i', '--iteration',
+                        help='The number of iterations to run the test over',
+                        default=100)
+    parser.add_argument('-v', '--verbose', dest="verbose", default=False,
+                        action='store_true',
+                        help='Make script execution more verbose')
+    args = parser.parse_args()
+
+    # Call 'sw-912-driver.py' with T2-specific options
+    cmd = './sw-912-driver.py '
+    # User command-line options
+    cmd += '{} '.format(args.host)
+    if args.baudrate:
+        cmd += '-r {} '.format(args.baudrate)
+    if args.size:
+        cmd += '-s {} '.format(args.size)
+    if args.iteration:
+        cmd += '-i {} '.format(args.iteration)
+    if args.verbose:
+        cmd += '-v'.format(args.baudrate)
+    # T2-specific options
+    cmd += ' --ap'
+    cmd += ' -t sink'
+    cmd += ' -b APB2 APB3'
+    cmd += ' -c T2'
+
+    if args.verbose:
+        print(cmd)
+    system(cmd)
+
+if __name__ == '__main__':
+    main()
